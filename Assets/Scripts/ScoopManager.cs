882efbfb5bc1672f30edd49858e82840c53535d0
Ryan Munin <ryan.munin@gmail.com>
2022-09-13 09:51:53 -0700

    Bug fix for double count problem

--- Assets/Scripts/ScoopManager.cs
+++ Assets/Scripts/ScoopManager.cs
@@ -7,14 +7,28 @@ public class ScoopManager : MonoBehaviour
 
     [SerializeField] int scoreVal;
     private GameManager gameManager;
+    private bool isColliding;
 
     private void Start()
     {
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        isColliding = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (isColliding) return;
+        isColliding = true;
+
+        StartCoroutine(ResetCollide());
+
+        Debug.Log("Value passed: " + scoreVal);
         gameManager.UpdateScore(scoreVal);
+    }
+
+    private IEnumerator ResetCollide()
+    {
+        yield return new WaitForEndOfFrame();
+        isColliding = false;
     }
 }
